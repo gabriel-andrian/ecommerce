943169eff82558cae9074e68a3a90d3c84c9ade2
Paulo Santos <pauloeduardo_77@hotmail.com>
2021-03-02 22:50:52 +0000

    Paulo

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'rest_framework',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
@@ -76,7 +79,7 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.ecommerce',
     }
 }
 
