6f8503d0a6b948341d3489a1279ff32f6a01d699
Gabriel Vieira Andrian <gabriel_andrian@hotmail.com>
2021-04-20 15:50:13 -0300

    feat: url product with serializer

--- products/models.py
+++ products/models.py
@@ -25,6 +25,7 @@ class Product(models.Model):
     slug = models.SlugField(default="")
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=10)
+    url = models.CharField(max_length=255)
     created = models.DateField(auto_now_add=True)
     modified = models.DateField(auto_now=True)
 
